a0a29344db50c108c38b6df696d8b1b4d4e29739
Aleksandra.Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-03-04 20:20:24 +0000

    Introduced event class

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
+class Event {
+  constructor(eventDesc) {
+    this.eventDesc = eventDesc;
+  }
+  emit() {
+    zooSingleton.events.push(this.eventDesc);
+  }
+}
 class Animal {
   constructor(id, kind, name) {
     this.id = id;
     this.kind = kind;
     this.name = name;
+  }
+  eat(food) {}
+}
+class Cat extends Animal {
+  constructor(id, kind, name) {
+    super(id, kind, name);
+  }
+  eat(meat) {
+    let eatMeat = new Event(kind + " named " + name + " ate " + meat);
+    eatMeat.emit();
+  }
+}
+class Tiger extends Cat {
+  constructor(id, kind, name) {
+    super(id, kind, name);
+  }
+}
+class Lion extends Cat {
+  constructor(id, kind, name) {
+    super(id, kind, name);
+  }
+}
+class Monkey extends Animal {
+  constructor(id, kind, name) {
+    super(id, kind, name);
+  }
+  eat(fruit) {
+    let eatFruit = new Event(kind + " named " + name + " ate " + fruit);
+    eatFruit.emit();
+  }
+}
+class Baboon extends Monkey {
+  constructor(id, kind, name) {
+    super(id, kind, name);
+  }
+}
+class Chimpanzee extends Monkey {
+  constructor(id, kind, name) {
+    super(id, kind, name);
   }
 }
 class Employee {
@@ -16,18 +63,21 @@ class Feeder extends Employee {
     super(id, name);
     this.role = role;
   }
+  feed(animal) {}
 }
 class Traineer extends Employee {
   constructor(id, role, name) {
     super(id, name);
     this.role = role;
   }
+  train(animal, trick) {}
 }
 class ZOO {
   constructor(name) {
     this.name = name;
     this.animals = [];
     this.employees = [];
+    this.events = [];
   }
   addAnimal(animal) {}
   removeAnimal(animal) {}
